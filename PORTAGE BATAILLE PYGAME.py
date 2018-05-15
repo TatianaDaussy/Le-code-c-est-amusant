dd8121c0c25262f525bc1f974b52573cee2943ee
TatianaDaussy <38993845+TatianaDaussy@users.noreply.github.com>
2018-05-15 21:29:03 +0200

    Click on the boat
    C'est comme ça qu'on fait ?  xD
    
    Au début j'étais partit sur une bataille navale, mais j'ai changé d'idée pour un début, je voulais faire un jeu où en gros quand tu cliques sur le bâteau, il change de position etc. 
    BREF, c'est loin d'être terminé, et deux gros problèmes, le pygame plante dès l'ouverture, et, quand je clique sur le bâteau, ça ne fait... Absolument rien xD

---
+++ PORTAGE BATAILLE PYGAME.py
@@ -0,0 +1,31 @@
+﻿import pygame
+from pygame.locals import *
+pygame.init()
+
+fenetre = pygame.display.set_mode((626,417))
+
+fond = pygame.image.load("background.jpg").convert()
+fenetre.blit(fond, (0,0))
+
+import random
+x_random = random.randint(100,400)
+y_random = random.randint(30,300)
+boat = pygame.image.load("boat.png").convert_alpha()
+position_boat = fenetre.blit(boat, (x_random, y_random))
+
+pygame.display.flip()
+
+
+continuer = 1
+
+while continuer:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            continuer = 0
+        if event.type == MOUSEBUTTONDOWN:
+            if boat.collidepoint("boat.png"):
+                position_boat = fenetre.blit(boat, (x_random, y_random))
+
+fenetre.blit(fond, (0,0))
+
+pygame.display.flip()
